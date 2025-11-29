a409f5b90231d35f9f174161fe7605af6ffbaf4b
JoviOakz <joao.mateussdc@gmail.com>
2025-11-28 22:18:38 -0300

    fix

--- src/models/user.js
+++ src/models/user.js
@@ -2,22 +2,9 @@ import sequelize from "../config/database.js";
 import { DataTypes } from "sequelize";
 
 const User = sequelize.define("User", {
-  username: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true
-  },
-  password: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  role: {
-    type: DataTypes.ENUM("user", "admin"),
-    allowNull: false,
-    defaultValue: "user"
-  }
-}, {
-  tableName: "users"
-});
+  username: { type: DataTypes.STRING, allowNull: false, unique: true },
+  password: { type: DataTypes.STRING, allowNull: false },
+  role: { type: DataTypes.ENUM("user", "admin"), allowNull: false, defaultValue: "user" }
+}, { tableName: "users" });
 
 export default User;
